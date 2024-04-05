e6d16a96d96d3ba3d89f2ce1b301264430757268
Athula1996 <110589162+Athula1996@users.noreply.github.com>
2024-04-05 12:19:26 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,18 +11,18 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('10389378'))
     API_HASH = str(getenv('cdd5c820cb6abeecaef38e2bb8db4860'))
-    BOT_TOKEN = str(getenv('6298021090:AAEyF6rVpR4X4X3pKMsUDrHIvIJsjKqp1yU'))
+    BOT_TOKEN = str(getenv('6627128042:AAHWBTqCLeIMKSOLkE4D9xgrmnFGYY3iR-k'))
     name = str(getenv('name', 'Pdisk_Link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('-1001524622686'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://able-mommy-as772685.koyeb.app'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://u1copy-6e29fcad33e4.herokuapp.com/'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1938030055").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('@Akash5213'))
+    OWNER_USERNAME = str(getenv('Akash5213'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('mongodb+srv://akash:official@cluster0.iwbsajx.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('mongodb+srv://reddead:deadred@cluster0.btdcf9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('https://t.me/+HOkpWZciAKA5NGQ1', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
